af0e2871551728d14ea677eea7a0aef67a9afc31
fgh23387736 <23387736@qq.com>
2018-11-26 22:47:58 +0800

    学习完路由

--- laravel/blog/routes/web.php
+++ laravel/blog/routes/web.php
@@ -18,3 +18,30 @@ Route::get('/', function () {
 Route::get('hello', function() {
 	return '鈜鉝';
 });
+
+Route::get('user/profile', function() {
+	return 'my url ' . route('profile');
+})->name('profile');
+
+Route::get('user/{id}/profile', function($id) {
+	$url = route('profile', ['id' => $id]);
+	return $url;
+})->name('profile');
+
+Route::domain('{account}.learning.test')->group(function () {
+	Route::get('user/{id}', function ($account, $Id)
+	{
+		return 'This is ' . $account . ' page of User ' . $id;
+	});
+});
+
+Route::get('test/Route', function () {
+	// 获取当前路由实例
+	// $route = Route::current(); 
+	// 获取当前路由名称
+	$name = Route::currentRouteName();
+	// 获取当前路由action属性
+	$action = Route::currentRouteAction();
+
+	return ' ' . $name . ',' . $action;
+})->name('theUrl');
